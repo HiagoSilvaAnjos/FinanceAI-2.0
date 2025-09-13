643eb76b82056a5f874b256c733a5d52e8204705
HiagoSilva <hiagosilvaanjos7@gmail.com>
2025-09-13 19:16:48 -0300

    chore: add , , and  setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="">Finance AI 2.0</h1>
+      <Button className="my-1 px-5 text-red-300">Say click me</Button>
+    </div>
+  );
+}
