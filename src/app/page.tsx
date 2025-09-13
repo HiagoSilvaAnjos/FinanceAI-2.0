2dcfc17f213a6a12ce458ef3294478ae320d4150
HiagoSilva <hiagosilvaanjos7@gmail.com>
2025-09-13 19:27:46 -0300

    chore: add styles themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="">Finance AI 2.0</h1>
-      <Button className="my-1 px-5 text-red-300">Say click me</Button>
+      <Button className="">Say click me</Button>
     </div>
   );
 }
